41b5cdb217335e5de830cfc6b79c2ee1d7bf7616
Butvila_Antanas <antanas.werwolf@gmail.com>
2023-12-18 17:59:40 +0300

    Added third function to print the result

--- Program.cs
+++ Program.cs
@@ -32,3 +32,20 @@ string [] LessThenThreeOrEqual(string [] array)
   }
   return resultArray;
 }
+
+void PrintArray(string [] array)
+{
+     Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+    Console.Write($"{array[i]}");
+
+    if (i < array.Length - 1)
+    {
+     Console.Write(", ");
+    }
+     }
+     Console.Write("]");
+}
+GetArray(array);
+PrintArray(LessThenThreeOrEqual(array));
