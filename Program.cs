dcfca0306d8c4de8be9833479fb7c728df326b46
Butvila_Antanas <antanas.werwolf@gmail.com>
2023-12-18 17:56:27 +0300

    Added first function and input data

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите количество элементов массива: ");
+
+int size = Convert.ToInt32(Console.ReadLine());
+
+string [] array = new string [size];
+void GetArray(string [] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+     Console.WriteLine($"Введите {i+1} элемент массива");
+     array[i] = Console.ReadLine();
+  }
+}
